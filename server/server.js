6e9c614e5a2cde353876dbf5f881b34a94aad910
diana.reyes <ceciliatisca@gmail.com>
2020-12-16 18:56:51 -0600

    Creación de la carpeta de rutas

--- server/server.js
+++ server/server.js
@@ -11,56 +11,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
   
 app.get('/', function (req, res) {
-  res.send('Hola mundo')
-})
- 
-app.get('/usuario', function (req, res) {
-    res.json({
-        ok: 200,
-        msg: 'Usuarios consultados con exito'
-    })
+    res.send('Hola mundo')
   })
 
-app.post('/usuario', function(req, res){
-    let nombre = req.body.nombre;
-    let body = req.body;
-    
-    if(nombre === undefined){
-        res.status(400).json({
-            ok: 400,
-            msg: 'Es obligatorio insertar el nombre'
-        }) 
-    }else {
-        res.json({
-            ok: 200,
-            msg: 'Usuario insertado con exito',
-            body:body
-        }) 
-    }  
-})
-
-app.put('/usuario/:id/:nombre', function(req, res){
-    let id = req.params.id
-    let nombre = req.params.nombre
-
-        res.json({
-            ok: 200,
-            msg: 'Usuario actualizado con exito',
-            id: id,
-            nombre:nombre
-        }) 
-})
-
-app.delete('/usuario', function(req, res){
-    let id = req.params.id;
-
-    res.json({
-        ok: 200,
-        msg: 'Usuario eliminado correctamente',
-        id:id
-    })
-})
-
+  app.use(require('./routes/usuario'));
+  
+   
  mongoose.connect('mongodb://localhost:27017/empresa', {
   useNewUrlParser: true,
   useUnifiedTopology: true, 
