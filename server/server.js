a67b40319b861195ad9a3a1642f267e9881028a1
diana.reyes <ceciliatisca@gmail.com>
2020-12-16 18:25:56 -0600

    Conexion lista de la base de datos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config')
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
 
@@ -60,6 +61,16 @@ app.delete('/usuario', function(req, res){
     })
 })
 
+ mongoose.connect('mongodb://localhost:27017/empresa', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+  useFindAndModify: false,
+  useCreateIndex: true
+ }, (err, res) => {
+    if(err) throw err;
+    console.log('Base de datos ONLINE')
+
+});
 
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta en linea por el puerto ', process.env.PORT)
